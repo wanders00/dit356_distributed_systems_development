08c7349cb254431860cedfc5e8bce9afc5f1bfd4
William Andersson <willand@student.chalmers.se>
2023-11-09 21:47:10 +0100

    #3 mqtt structure for middleware services

---
+++ Middleware/template/src/main/java/com/toothtrek/App.java
@@ -0,0 +1,27 @@
+package com.toothtrek;
+
+import com.toothtrek.mqtt.MqttHandler;
+
+public class App {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java -jar <jar-file> <broker-address> <client-id> <qos>");
+            System.exit(1);
+        }
+        String broker = args[0];
+        String clientId = args[1];
+        int qos = Integer.parseInt(args[2]);
+
+        MqttHandler handler = new MqttHandler(broker, clientId, qos);
+        handler.connect(true, true);
+
+        // *** Example usage ***
+        // String topic = "a/b/c";
+        // String content = "lorem ipsum";
+        // handler.subscribe(topic);
+        // handler.publish(topic, content);
+
+        // fyi: program will continue running, use ctrl+c to exit
+        // specify how to handle callbacks in /mqtt/MqttCallbackHandler.java
+    }
+}
