c00161bdec4e07e42467f6ba24f91e2635575d6d
William Andersson <willand@student.chalmers.se>
2023-11-28 22:40:23 +0100

    #22 interoperability with #11 co-auth: @vasilena
    To display working example on Milestone 2 meeting.
    
    Furthermore:
    Allows using response topic even if no mqtt message property.
    Instead use a "responseTopic" attribute in the json to provide it.

--- Middleware/bookings/src/main/java/com/toothtrek/bookings/request/timeslot/TimeslotCreateRequestHandler.java
+++ Middleware/bookings/src/main/java/com/toothtrek/bookings/request/timeslot/TimeslotCreateRequestHandler.java
@@ -1,17 +1,23 @@
 package com.toothtrek.bookings.request.timeslot;
 
 import org.eclipse.paho.mqttv5.common.MqttMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import com.toothtrek.bookings.request.RequestHandlerInterface;
+import com.toothtrek.bookings.response.ResponseHandler;
+import com.toothtrek.bookings.response.ResponseStatus;
 
 @Configuration
 public class TimeslotCreateRequestHandler implements RequestHandlerInterface {
 
+    @Autowired
+    private ResponseHandler responseHandler;
+
     @Override
     public void handle(MqttMessage request) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+        responseHandler.reply(ResponseStatus.SUCCESS, request);
+        // TODO: Implement
     }
 
 }
