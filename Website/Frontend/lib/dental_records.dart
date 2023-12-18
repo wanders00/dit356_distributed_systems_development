3f93cfe72a9c2edb1cd42754499ce51c4a2192d7
faundez <faundez@student.chalmers.se>
2023-12-18 21:52:31 +0100

    Refactor DentalRecords to use dynamic data #39

--- Website/Frontend/lib/dental_records.dart
+++ Website/Frontend/lib/dental_records.dart
@@ -8,119 +8,105 @@ class DentalRecordsPage extends StatefulWidget {
 }
 
 class DentalRecordsPageState extends State<DentalRecordsPage> {
+  //Dummy Data. Remove Later
+  List<String> recordDates = [
+    "2023-12-11",
+    "2023-05-23",
+    "2022-12-11",
+    "2022-05-15",
+    "2022-01-11",
+    "2021-12-11",
+    "2021-05-23",
+    "2021-01-11",
+    "2020-12-11",
+    "2020-05-23",
+    "2020-01-11",
+    "2019-12-11",
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      alignment: Alignment.center,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/records-background-dark.png'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          const Text("Dental Record",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              )),
-          const Text("*Patient Name*",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              )),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-              width: 600,
-              alignment: Alignment.center,
-              child: ExpansionTile(
-                collapsedBackgroundColor:
-                    Theme.of(context).colorScheme.primaryContainer,
-                collapsedTextColor:
-                    Theme.of(context).colorScheme.onPrimaryContainer,
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                textColor: Theme.of(context).colorScheme.onPrimary,
-                title: const Text("2023-12-11"),
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/records-background-dark.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  ListTile(
-                      title: Text("Notes:",
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                              fontWeight: FontWeight.bold)),
-                      subtitle: Text("Random Scribbles",
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.onPrimary))),
-                  ListTile(
-                    title: Text("- Doctor",
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.onPrimary)),
-                  )
-                ],
-              )),
-          Container(
-              width: 600,
-              alignment: Alignment.center,
-              child: ExpansionTile(
-                collapsedBackgroundColor:
-                    Theme.of(context).colorScheme.primaryContainer,
-                collapsedTextColor:
-                    Theme.of(context).colorScheme.onPrimaryContainer,
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                textColor: Theme.of(context).colorScheme.onPrimary,
-                title: const Text("2023-05-23"),
-                children: [
-                  ListTile(
-                      title: Text("Notes:",
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                              fontWeight: FontWeight.bold)),
-                      subtitle: Text("Random Scribbles",
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.onPrimary))),
-                  ListTile(
-                    title: Text("- Doctor",
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.onPrimary)),
-                  )
-                ],
-              )),
-          Container(
-              width: 600,
-              alignment: Alignment.center,
-              child: ExpansionTile(
-                collapsedBackgroundColor:
-                    Theme.of(context).colorScheme.primaryContainer,
-                collapsedTextColor:
-                    Theme.of(context).colorScheme.onPrimaryContainer,
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                textColor: Theme.of(context).colorScheme.onPrimary,
-                title: const Text("2022-12-11"),
-                children: [
-                  ListTile(
-                      title: Text("Notes:",
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                              fontWeight: FontWeight.bold)),
-                      subtitle: Text("Random Scribbles",
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.onPrimary))),
-                  ListTile(
-                    title: Text("- Doctor",
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.onPrimary)),
+                  const SizedBox(
+                    height: 50,
                   ),
-                ],
-              ))
-        ],
-      ),
-    ));
+                  const Text("Dental Record",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  const Text("*Patient Name*",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                      child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ...generateExpansionTiles(),
+                      ],
+                    ),
+                  ))
+                ])));
+  }
+
+  List<Widget> generateExpansionTiles() {
+    return recordDates.map((date) {
+      return Container(
+        width: 600,
+        alignment: Alignment.center,
+        child: ExpansionTile(
+          iconColor: Theme.of(context).colorScheme.onPrimary,
+          collapsedBackgroundColor:
+              Theme.of(context).colorScheme.primaryContainer,
+          collapsedTextColor: Theme.of(context).colorScheme.onPrimaryContainer,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          textColor: Theme.of(context).colorScheme.onPrimary,
+          title: Text(date),
+          children: [
+            ListTile(
+              title: Text(
+                "Notes:",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                "Random Scribbles",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                "- Doctor",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
   }
 }
