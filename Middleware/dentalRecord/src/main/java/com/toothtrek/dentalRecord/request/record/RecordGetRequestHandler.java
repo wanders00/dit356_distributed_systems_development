75644b75da65fe60cebbdbcfb67343ec61c8ffce
Vasilena Karaivanova <vasilena@student.chalmers.se>
2023-12-21 17:07:46 +0100

    #27 Added get & update for dental record
    Co-authored by: @willand

--- Middleware/dentalRecord/src/main/java/com/toothtrek/dentalRecord/request/record/RecordGetRequestHandler.java
+++ Middleware/dentalRecord/src/main/java/com/toothtrek/dentalRecord/request/record/RecordGetRequestHandler.java
@@ -1,17 +1,63 @@
 package com.toothtrek.dentalRecord.request.record;
 
+import java.sql.Timestamp;
+
 import org.eclipse.paho.mqttv5.common.MqttMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import com.toothtrek.dentalRecord.repository.RecordRepository;
 import com.toothtrek.dentalRecord.request.RequestHandlerInterface;
+import com.toothtrek.dentalRecord.response.ResponseHandler;
+import com.toothtrek.dentalRecord.response.ResponseStatus;
+import com.toothtrek.dentalRecord.serializer.json.TimestampSerializer;
+import com.toothtrek.dentalRecord.entity.Record;
 
 @Configuration
 public class RecordGetRequestHandler implements RequestHandlerInterface {
 
+    @Autowired
+    private RecordRepository recordRepository;
+
+    @Autowired
+    private ResponseHandler responseHandler;
+
     @Override
     public void handle(MqttMessage request) {
-        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+        try {
+            JsonObject json;
+            try {
+                json = new Gson().fromJson(new String(request.getPayload()), JsonObject.class);
+            } catch (JsonSyntaxException e) {
+                responseHandler.reply(ResponseStatus.ERROR, "Invalid JSON", request);
+                return;
+            }
+
+            // Get the record ID from the JSON
+            Long id = json.get("id").getAsLong();
+
+            // Find the record & check if it exists
+            Record record;
+            try {
+                record = recordRepository.findById(id).get();
+            } catch (Exception e) {
+                responseHandler.reply(ResponseStatus.ERROR, "Record does not exist", request);
+                return;
+            }
+
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(Timestamp.class, new TimestampSerializer())
+                    .create();
+            // Reply with the record
+            responseHandler.reply(ResponseStatus.SUCCESS, gson.toJson(record), request);
+
+        } catch (JsonSyntaxException e) {
+            // Handle the exception
+            e.printStackTrace();
+        }
     }
-
 }
-
---
+++ Middleware/dentalRecord/src/main/java/com/toothtrek/dentalRecord/serializer/json/TimestampSerializer.java
@@ -0,0 +1,32 @@
+package com.toothtrek.dentalRecord.serializer.json;
+
+import java.lang.reflect.Type;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+/**
+ * Custom serializer for Timestamp class.
+ * <p>
+ * Necessary to use when converting Timeslots dateAndTime attribute (Timestamp)
+ * to JSON when sending data to the frontend.
+ * As the frontend requires the date and time to be in the format of:
+ * <p>
+ * "yyyy-MM-dd HH:mm:ss".
+ * 
+ * @see com.toothtrek.bookings.entity.Timeslot
+ * @see java.sql.Timestamp
+ */
+public class TimestampSerializer implements JsonSerializer<Timestamp> {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public JsonElement serialize(Timestamp src, Type typeOfSrc, JsonSerializationContext context) {
+        String formattedDate = dateFormat.format(src);
+        return new JsonPrimitive(formattedDate);
+    }
+}
